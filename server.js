f3b4ab41e4a983d960f23781bd10d98ecf431833
PARGAT SINGH <107990987+pargat1204@users.noreply.github.com>
2022-11-11 08:46:26 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,22 @@
+let bodyparser = require('body-parser');
+let express = require("express");
+let path = require('path');
+let app = express();
+let PORT = process.env.port || 3000;
+app.set("views", path.join(__dirname));
+app.set("view engine", "ejs");
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
+
+app.get("/", function (req, res) {
+    res.render("index");
+});
+
+app.post('/agent', (req, res) => {
+    console.log(req.body);
+});
+
+app.listen(PORT, function (error) {
+    if (error) { throw error; };
+    console.log("Server created Successfully on PORT", PORT);
+})
